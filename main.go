56807c9cde052424252ee17544ef4bacb24faaa6
vesko972 <dust9722@gmail.com>
2024-08-04 09:23:25 -0700

    version: 1.3

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	r.Use(middleware.LoggingMiddleware)
 
 	r.GET("/version", func(c *gin.Context) {
-		c.String(http.StatusOK, "This is the version 1.2 of the backend app")
+		c.String(http.StatusOK, "This is the version 1.3 of the backend app")
 	})
 
 	apiGroup := r.Group("/api")
