92664542ba741299ec15bdb4eeb5863b46a113d7
vesko972 <dust9722@gmail.com>
2024-08-31 18:45:49 +0300

    done

--- middleware/corsCSP.go
+++ middleware/corsCSP.go
@@ -1,15 +1,14 @@
 package middleware
 
-
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
-
 func CORSandCSP() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
